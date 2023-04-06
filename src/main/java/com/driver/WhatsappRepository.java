cd0f020517820b2e260b33d99ed3a5c93e342d35
Merlin Mon Mathew <merlinmonmathew@gmail.com>
2023-04-06 15:59:11 +0530

    second

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -102,7 +102,7 @@ public class WhatsappRepository {
 
     public int createMessage(String content){
         messageId++;
-        Message message = new Message(messageId,content,new Date());
+        Message message = new Message(messageId,content);
         return messageId;
     }
 
